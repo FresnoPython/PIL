7e62bd4b5378b43d044fee325eb7321da57fe718
Allen Mills <Mills.Allen.James@gmail.com>
2017-06-24 15:36:40 -0700

    chore: update doc format

--- utility/write_rgb_image.py
+++ utility/write_rgb_image.py
@@ -3,11 +3,10 @@ from PIL import Image as i
 
 def write_rgb_image(colors, bounds, background_color=(0, 0, 0), should_save=False, filename='image.png'):
     '''
-    write_rgb_image(
+    Arguments:
         colors, List of {x,y,color} where color is a (red, green, blue) tuple
         bounds, (width, height) tuple
         background_color, (red, green, blue) tuple to determine base color. Defaults to (0, 0, 0)
-    )
     '''
 
     image = i.new('RGB', bounds, background_color)
